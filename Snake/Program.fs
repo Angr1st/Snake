d52e0b74fe9808456e47bd1a7188b804c248f642
Torben Clasen <t.clasen@outlook.com>
2020-09-07 23:04:02 +0200

    Init the GlobalGameState

--- Snake/Program.fs
+++ Snake/Program.fs
@@ -17,8 +17,8 @@ type Direction =
 
 type GameField =
     {
-    X:int
-    Y:int
+    X:int //left and right
+    Y:int //up and down
     MoveDirection:Direction
     }
 
@@ -37,14 +37,15 @@ type GameFieldType =
     | RightThreeWay of StaticField
     | UpperRightCorner of StaticField
     | LowerRightCorner of StaticField
-    | Empty of StaticField
+    | Empty
     | Apple of GameField
     | SnakeHead of GameField
     | SnakeBody of GameField
+    | ScoreField of char
     interface IDisplayable with
         member g.Display() =
             match g with
-            | Empty _-> " "
+            | Empty -> " "
             | Apple _-> "O"
             | SnakeHead _-> "■"
             | SnakeBody _-> "■"
@@ -56,15 +57,31 @@ type GameFieldType =
             | LowerRightCorner _-> "╝"
             | LeftThreeWay _-> "╣"
             | RightThreeWay _-> "╠"
+            | ScoreField c-> c.ToString()
 
-type GameState = 
+type GameState = {MatchField:GameFieldType[,]}
+
+type ScoreBoard = {ScoreFields:GameFieldType[]}
+
+type GlobalGameState = 
     {
     Score:int
-    Matchfield:GameFieldType[,]
-    }   
+    CompleteMatchField:GameFieldType[,]
+    Matchfield:GameState
+    ScoreArea:ScoreBoard
+    } 
 
+let initGame = 
+    let completeGameField = Array2D.create 17 19 Empty
+    let scoreArea = completeGameField.[1..15,1]
+    let matchField = completeGameField.[1..15,4..17]
+    {
+    Score = 0
+    CompleteMatchField = completeGameField
+    Matchfield = {MatchField=matchField}
+    ScoreArea = {ScoreFields= scoreArea}
+    }
 [<EntryPoint>]
 let main argv =
     
-    printfn "Hello World from F#!"
     0 // return an integer exit code
